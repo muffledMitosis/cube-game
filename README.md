b290b90ee99f11885a2fc376a65f4f993b9f02bc
Meth Munindradasa <methsndr@gmail.com>
2022-02-02 01:17:00 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cube Game
+
+A Minecraft clone I wrote to get a toy game engine up and running.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	std::cout<< "Build system works" << std::endl;
+	return 0;
+}
